3c6ec76b33fbc20f1a33d790ed3be5eac8eeaa8d
Darío <dariotoro@MacBook-Pro-de-Dario.local>
2019-12-14 12:40:01 -0300

    fixed message

--- src/main/java/com/dario/project23people/validator/StudentRutConstraint.java
+++ src/main/java/com/dario/project23people/validator/StudentRutConstraint.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface StudentRutConstraint {
-    String message() default "Invalid phone number";
+    String message() default "Invalid rut";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
